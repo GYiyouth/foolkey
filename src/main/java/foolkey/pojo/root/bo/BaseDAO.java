4352fbd4f45cc3966e2f591f66d6a71949c6984b
耿瑞 <gr2016@mail.ustc.edu.cn>
2017-04-25 16:20:49 +0800

    BaseDAO

---
+++ src/main/java/foolkey/pojo/root/bo/BaseDAO.java
@@ -0,0 +1,37 @@
+package foolkey.pojo.root.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by admin on 2017/4/25.
+ */
+@Resource
+@Transactional
+public class BaseDAO<T> {
+
+    @Autowired
+    private HibernateTemplate hibernateTemplate;
+
+    public T get(Class<T> entityClazz, Serializable id) {
+        return hibernateTemplate.get(entityClazz,id);
+    }
+
+    public void save(T entity) {
+        hibernateTemplate.save(entity);
+    }
+
+    public void update(T entity) {
+        hibernateTemplate.update(entity);
+    }
+
+    public void delete(T entity) {
+        hibernateTemplate.delete(entity);
+    }
+
+}
