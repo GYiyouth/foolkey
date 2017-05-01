803f388e35fff1d4f0da7d0cc32f5a0667cc0ca6
葛尧 <geyao2012@gmail.com>
2017-05-01 12:32:50 +0800

    token生成器，以后的生成统一调用这个方法

---
+++ src/main/java/foolkey/tool/TokenCreator.java
@@ -0,0 +1,11 @@
+package foolkey.tool;
+
+/**
+ * Created by geyao on 2017/5/1.
+ */
+public class TokenCreator {
+
+    public static String createToken(String str1, String str2){
+        return str1 + str2;
+    }
+}
