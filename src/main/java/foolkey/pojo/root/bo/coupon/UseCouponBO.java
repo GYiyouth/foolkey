be717745a4a1471d591d32b22d5ff0c424fd8641
葛尧 <geyao2012@gmail.com>
2017-05-01 19:41:50 +0800

    使用deadTime字段判断优惠券是否过期

--- src/main/java/foolkey/pojo/root/bo/coupon/UseCouponBO.java
+++ src/main/java/foolkey/pojo/root/bo/coupon/UseCouponBO.java
@@ -1,10 +1,13 @@
 package foolkey.pojo.root.bo.coupon;
 
+import foolkey.pojo.root.vo.assistObject.CouponTypeEnum;
 import foolkey.pojo.root.vo.dto.CouponDTO;
 import foolkey.pojo.root.vo.dto.OrderBuyCourseDTO;
 import foolkey.pojo.root.vo.dto.StudentDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * Created by geyao on 2017/5/1.
@@ -13,8 +16,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UseCouponBO {
 
-    public void userCoupon(
+    /**
+     * 使用优惠券，验证优惠券的可用性，返回计算后价格
+     * @param studentDTO
+     * @param orderDTO
+     * @param couponDTO
+     */
+    public Double userCoupon(
             StudentDTO studentDTO, OrderBuyCourseDTO orderDTO, CouponDTO couponDTO){
 
+        if (couponDTO == null)
+            return orderDTO.getAmount();
+        if (!couponDTO.getId().equals( studentDTO.getId() ))
+            return null;
+        if (couponDTO.getDeadTime().compareTo(new Date()) < 0) // 优惠券已使用或者过期
+            return null;
+        if (couponDTO.getCouponTypeEnum().compareTo(CouponTypeEnum.购买课程) == 0
+                || couponDTO.getCouponTypeEnum().compareTo(CouponTypeEnum.全场) == 0){
+            int time = couponDTO.getDeadTime().compareTo( new Date() );
+            if (time > 0){
+                int level = couponDTO.getLevel().compareTo(orderDTO.getAmount());
+                if (level < 0) {
+                    orderDTO.setCouponId(couponDTO.getId());
+                    return (orderDTO.getAmount() - couponDTO.getValue());
+                }
+            }
+        }
+        return null;
     }
 }
