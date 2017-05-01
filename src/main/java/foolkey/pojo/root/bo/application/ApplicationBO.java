01119d19ee5f6c84688d59104fe60e01e7075d35
葛尧 <geyao2012@gmail.com>
2017-05-01 13:47:37 +0800

    新建对老师课程的申请

---
+++ src/main/java/foolkey/pojo/root/bo/application/ApplicationBO.java
@@ -0,0 +1,32 @@
+package foolkey.pojo.root.bo.application;
+
+import foolkey.pojo.root.DAO.application_teacher_course.SaveApplicationTeacherCourseDAO;
+import foolkey.pojo.root.vo.dto.ApplicationTeacherCourseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by geyao on 2017/5/1.
+ */
+@Service
+@Transactional
+public class ApplicationBO {
+    @Autowired
+    private SaveApplicationTeacherCourseDAO dao;
+
+    /**
+     * 生成对老师课程的申请消息
+     * @return
+     */
+    public ApplicationTeacherCourseDTO createApplicationForTeacherCourse(
+            Long applicantId,
+            Long courseId
+    ){
+        ApplicationTeacherCourseDTO application = new ApplicationTeacherCourseDTO();
+        application.setApplicantId(applicantId);
+        application.setCourseTeacherId(courseId);
+        dao.save(application);
+        return application;
+    }
+}
