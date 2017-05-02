9b6a01dd8af5fc156f6bc79f7d81eaab063d9edd
葛尧 <geyao2012@gmail.com>
2017-05-02 19:02:04 +0800

    发布悬赏，改成了json传输

--- src/main/java/foolkey/controller/CourseStudent/PublishCourseStudentController.java
+++ src/main/java/foolkey/controller/CourseStudent/PublishCourseStudentController.java
@@ -8,12 +8,14 @@ import foolkey.pojo.root.vo.assistObject.*;
 import foolkey.pojo.root.vo.dto.CourseStudentDTO;
 import foolkey.pojo.root.vo.dto.CourseTeacherDTO;
 import foolkey.pojo.root.vo.dto.StudentDTO;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -34,18 +36,38 @@ public class PublishCourseStudentController extends AbstractController {
 
     @RequestMapping(value = "/publishRewardCourse")
     public void execute(
-            @RequestParam("token") String token,
-            @RequestParam("technicTagEnum")TechnicTagEnum technicTagEnum,
-            @RequestParam("topic")String topic,
-            @RequestParam("description")String description ,
-            @RequestParam("price")Double price,
-            @RequestParam("courseTimeDayEnum")CourseTimeDayEnum courseTimeDayEnum,
-            @RequestParam("teachMethodEnum")TeachMethodEnum teachMethodEnum,
-            @RequestParam("teacherRequirementEnum")TeacherRequirementEnum teacherRequirementEnum,
-            @RequestParam("studentBaseEnum")StudentBaseEnum studentBaseEnum,
+            HttpServletRequest request,
+//            @RequestParam("token") String token,
+//            @RequestParam("technicTagEnum")TechnicTagEnum technicTagEnum,
+//            @RequestParam("topic")String topic,
+//            @RequestParam("description")String description ,
+//            @RequestParam("price")Double price,
+//            @RequestParam("courseTimeDayEnum")CourseTimeDayEnum courseTimeDayEnum,
+//            @RequestParam("teachMethodEnum")TeachMethodEnum teachMethodEnum,
+//            @RequestParam("teacherRequirementEnum")TeacherRequirementEnum teacherRequirementEnum,
+//            @RequestParam("studentBaseEnum")StudentBaseEnum studentBaseEnum,
             HttpServletResponse response
     ) throws Exception {
         try {
+            String clearText = request.getParameter("clearText");
+            JSONObject clearJSON = JSONObject.fromObject(clearText);
+
+            String token =clearJSON.getString("token");
+            String technicTagStr = clearJSON.getString("technicTagEnum");
+                TechnicTagEnum technicTagEnum = TechnicTagEnum.valueOf(technicTagStr);
+            String topic = clearJSON.getString("topic");
+            String description = clearJSON.getString("description");
+            Double price = clearJSON.getDouble("price");
+            String courseTimeDayStr = clearJSON.getString("courseTimeDayEnum");
+                CourseTimeDayEnum courseTimeDayEnum = CourseTimeDayEnum.valueOf(courseTimeDayStr);
+            String teachMethodStr = clearJSON.getString("teachMethodEnum");
+                TeachMethodEnum teachMethodEnum = TeachMethodEnum.valueOf(teachMethodStr);
+            String teacherRequirementStr = clearJSON.getString("teachRequirementEnum");
+                TeacherRequirementEnum teacherRequirementEnum = TeacherRequirementEnum.valueOf(teacherRequirementStr);
+//            String studentBase = clearJSON.getString("studentBaseEnum");
+//                StudentBaseEnum studentBaseEnum = StudentBaseEnum.valueOf(studentBase);
+
+
             StudentDTO studentDTO = studentInfoBO.getStudentDTO(token);
             courseStudentDTO.setCreatorId(studentDTO.getId());
             courseStudentDTO.setTechnicTagEnum(technicTagEnum);
@@ -55,7 +77,7 @@ public class PublishCourseStudentController extends AbstractController {
             courseStudentDTO.setCourseTimeDayEnum(courseTimeDayEnum);
             courseStudentDTO.setTeachMethodEnum(teachMethodEnum);
             courseStudentDTO.setTeacherRequirementEnum(teacherRequirementEnum);
-            courseStudentDTO.setStudentBaseEnum(studentBaseEnum);
+//            courseStudentDTO.setStudentBaseEnum(studentBaseEnum);
             courseStudentDTO.setCourseStudentStateEnum(CourseStudentStateEnum.待接单);
             courseStudentBO.publishCourseStudent(courseStudentDTO);
             jsonObject.put("result","success");
