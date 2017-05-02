797c0cc3c9e92ce417418c8f805c72e96cf3d68c
葛尧 <geyao2012@gmail.com>
2017-05-02 21:56:42 +0800

    极其简易地搜索

--- src/main/java/foolkey/pojo/root/bo/search/SearchCourseBO.java
+++ src/main/java/foolkey/pojo/root/bo/search/SearchCourseBO.java
@@ -1,5 +1,6 @@
 package foolkey.pojo.root.bo.search;
 
+import foolkey.pojo.root.DAO.course_teacher.GetCourseTeacherDAO;
 import foolkey.pojo.root.bo.CourseTeacher.CourseTeacherBO;
 import foolkey.pojo.root.vo.assistObject.CourseTimeDayEnum;
 import foolkey.pojo.root.vo.assistObject.TechnicTagEnum;
@@ -17,11 +18,24 @@ import java.util.List;
 public class SearchCourseBO {
     @Autowired
     private CourseTeacherBO courseTeacherBO;
+    @Autowired
+    private GetCourseTeacherDAO getCourseTeacherDAO;
 
-    public List<CourseTeacherDTO> searchCourseDTO(ArrayList<String> keyList, ArrayList<TechnicTagEnum> techList, ArrayList<CourseTimeDayEnum>timeList, int pageNo){
+    public List<CourseTeacherDTO> searchCourseDTO(
+            ArrayList<String> keyList
+            , ArrayList<TechnicTagEnum> techList
+            , ArrayList<CourseTimeDayEnum>timeList
+            , int pageNo
+    ) throws Exception{
 
         //如果只有技术关键字，则从缓存中取
-        if (keyList.size() == 0 && timeList.size() == 0 && )
+        if (keyList.size() == 0 && timeList.size() == 0 && techList.size() > 0)
+            return courseTeacherBO.getPopularCourseTeacher(techList.get(0), pageNo, 20);
+
+        //如果，有技术关键词
+        if (keyList.size() > 0){
+            return getCourseTeacherDAO.findByPage("from CourseTeacherDTO ct where ct.topic like'%?%' ", pageNo, 20, keyList.get(0));
+        }
         return null;
     }
 }
