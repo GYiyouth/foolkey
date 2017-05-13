82b634e549e5a65d798d228d3c6d8f71695e4147
葛尧 <geyao2012@gmail.com>
2017-05-13 16:24:45 +0800

    评价课程和老师优化了一下

---
+++ src/main/java/foolkey/controller/course/judge/JudgeTeacherController.java
@@ -0,0 +1,28 @@
+package foolkey.controller.course.judge;
+
+import foolkey.controller.AbstractController;
+import foolkey.handler.course.judge.EvaluateTeacherHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by geyao on 2017/5/13.
+ */
+@RequestMapping("/judge")
+@Controller
+public class JudgeTeacherController extends AbstractController{
+
+    @Autowired
+    private EvaluateTeacherHandler evaluateTeacherHandler;
+    @RequestMapping("/teacher")
+    public void teacher(
+            HttpServletRequest request,
+            HttpServletResponse response
+    )throws Exception{
+        evaluateTeacherHandler.execute(request, response, jsonObject);
+    }
+}
