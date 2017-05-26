0abe9829c74e756396dd020fd41f430634d5937a
耿瑞 <gr2016@mail.ustc.edu.cn>
2017-05-26 23:38:43 +0800

    申请悬赏message

--- src/main/java/foolkey/pojo/root/bo/message/MessageBO.java
+++ src/main/java/foolkey/pojo/root/bo/message/MessageBO.java
@@ -25,7 +25,7 @@ public class MessageBO {
     ) throws Exception{
         String messagePayLoad = "messagePayLoad";
         String title = "有人申请了你的悬赏课程 ！";
-        String description = applicantDTO.getNickedName() + "申请了你的悬赏【" +
+        String description = "【" +
                 courseDTO.getTopic() + "】请，尽快查看吧~";
 
         return
